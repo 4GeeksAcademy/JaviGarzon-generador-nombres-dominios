726ea5fdec65a74a0373f758819fa1095b6e8d1f
javigarzon1 <javi.garzon@hotmail.com>
2025-01-15 13:15:00 +0000

    gen

---
+++ src/app.js
@@ -0,0 +1,14 @@
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extension = ['es','com','net']
+  iterar () {
+for(i=0, i<pronoum.lenght, i++){
+  for(j=0, j<adj.lenght, j++){
+    for(k=0, k<noum.lenght, k++){
+      for(l=0, l<extension.lenght, l++){
+
+console.log(pronoum[i],adj[j],noum[k],extension[l]) }}}}
+}
+
+
